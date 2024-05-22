851549c6f99f04b8d7cb7df4fe4d52ce5377fa75
Alberto Madalena Cativa <140245972+AlbertoCativa@users.noreply.github.com>
2024-05-22 14:46:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app=Flask("__main__")
 @app.get("/")
 def home():
     return {'msg':"ola mundo"}
+    
+@app.get("/users")
+def usuarios():
+    users=["Achelton","Paulo","Alberto","Antonio"]
+    return {"users":users}
+    
 
 
 if __name__=="__main__":
